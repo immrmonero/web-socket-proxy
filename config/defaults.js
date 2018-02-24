11476b60b5117964a130f77bef28b69305ef524d
Joseph, Jefree Sujit Eugene <JosephJ@advisory.com>
2018-02-24 09:50:38 +0530

    added web socket proxy

---
+++ config/defaults.js
@@ -0,0 +1,12 @@
+module.exports = {
+  host: "etn-pool.proxpool.com",
+  port: 3333,
+  pass: "x",
+  ssl: false,
+  address: "etnkEhFkLYk1r3DGFYc6nFKisTUbv51DxARtf6cZeXV8CqBwXaZhazGBpfEkEpAYEnTYCMwBKupddE5QZLBhNQsa5waXtghSwB",
+  user: null,
+  diff: null,
+  dynamicPool: false,
+  maxMinersPerConnection: 100,
+  donations: []
+};
